ee5361c555d39b049efda3b35387ed095480f0d7
taher19999 <94671425+taher19999@users.noreply.github.com>
2022-04-13 04:13:16 +0300

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,5 @@
 class Presets(object):
     PROCESS_TXT = "<code>جارٍ معالجة الرابط ..⏳</code>"
     UPLOAD_TXT = "<b>تحميل ملفك ..</b>"
-    ERROR_TXT = "<b>خطأ URL</b>\n\n<i>تعذر إنشاء ملف PDF باستخدام عنوان URL هذا.😔\nحاول مرة أخرى باستخدام صالح ..</i>"
     CAPTION_TXT = "{}\n\n<b>Developer(المطور):</b><a href='https://t.me/ta_ja199'><b> @ta_ja199 🧑🏻‍💻</b></a>"
     THUMB_URL = "https://telegra.ph/file/60706bd59c0829ed2f76f.jpg"
